b4ba0dfff3a2840372d3b0a3b115812885171c20
giwbie <irina.boldysh@gmail.com>
2019-01-24 23:12:30 +0100

    v.2

--- Task_1.java
+++ Task_1.java
@@ -1,19 +1,24 @@
 public class NextDate {
 
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        int x = 6;
+        int y = x++;
+        System.out.println("Y=" + y + " x: " + x);
 
         System.out.println(">>> Enter the YEAR: ");
         int year = reader.nextInt();
 
         System.out.println(">>> Enter the MONTH (1-12)");
         int month = reader.nextInt();
+
         if (month < 1 || month > 12) {
             System.out.println("!!!!!!!!! Entered month in INCORRECT. Program STOPPED");
             return;
         }
-        
+
         int monthNumDays = getMonthNumDays(year, month);
+
         System.out.println(">>> Enter the day of month (1-" + monthNumDays + "):");
         int day = reader.nextInt();
         if (day < 1 || day > monthNumDays) {
@@ -24,7 +29,6 @@ public class NextDate {
 
         String nextDate = getNextDate(year, month, day, monthNumDays);
         System.out.println("Entered date: " + day + "." + month + "." + year + ". Next date => " + nextDate);
-
     }
 
     private static int getMonthNumDays(int year, int month) {
@@ -40,8 +44,8 @@ public class NextDate {
                 }
                 return 28;
             default:
+                return 31;
         }
-        return 31;
     }
 
     private static String getNextDate(int year, int month, int day, int monthNumDays) {
